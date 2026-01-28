a5e54dce6396d8bdd97135e090f7c9f2971f5ab0
Johnston Antony <Johnston4u19@gmail.com>
2026-01-28 12:54:37 +0000

    created page model in Create.schtml.cs

---
+++ FinanceTracker/Pages/Transactions/Create.cshtml.cs
@@ -0,0 +1,37 @@
+using FinanceTracker.Data;
+using FinanceTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinanceTracker.Pages.Transactions;
+
+public class CreateModel : PageModel
+{
+    private readonly AppDbContext _db;
+
+    public CreateModel(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [BindProperty] //binds automatically to inputs.
+    public Transaction Transaction { get; set; } = new();
+
+    public void OnGet()
+    {
+        //empty to show form
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid) // valiation check if failled -> returns to main page.
+        {
+            return Page();
+        }
+
+        await _db.Transactions.AddAsync(Transaction);
+        await _db.SaveChangesAsync();
+
+        return RedirectToPage("/Transactions/Index"); // redirects to main page after saving transaction.
+    }
+}
