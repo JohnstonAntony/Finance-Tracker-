ec68b906183272d20fe733aa10cf7a3722dc0f6c
Johnston Antony <Johnston4u19@gmail.com>
2026-01-28 11:13:48 +0000

    initialised migration.

--- FinanceTracker/Program.cs
+++ FinanceTracker/Program.cs
@@ -3,7 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder services here
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
