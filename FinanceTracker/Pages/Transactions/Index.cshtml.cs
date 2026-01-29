88044bca059fbc4b459f16204e99efd2d63c1813
Johnston Antony <Johnston4u19@gmail.com>
2026-01-29 10:08:40 +0000

    Set up the backend for Transactions page. Made it sort in descending order for ease of use.

---
+++ FinanceTracker/Pages/Transactions/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using FinanceTracker.Data;
+using FinanceTracker.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceTracker.Pages.Transactions;
+
+public class IndexModel : PageModel // Razor page model for displaying transactions on request.
+{
+    private readonly AppDbContext _db;
+
+    public IndexModel(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public List<Transaction> Transactions { get; set; } = new(); 
+
+    public async Task OnGetAsync()
+    {
+        Transactions = await _db.Transactions
+            .OrderByDescending(t => t.Date) // Orders Transactions by Date in descending order.
+            .ToListAsync();
+    }
+}
